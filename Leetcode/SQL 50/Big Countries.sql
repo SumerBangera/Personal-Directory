9176b5f6ece9e1520457d0f15ef895f851c3ba09
Sumer Bangera <44096038+SumerBangera@users.noreply.github.com>
2023-10-02 10:09:51 +0800

    Create Big Countries.sql

---
+++ Leetcode/SQL 50/Big Countries.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000 OR population >= 25000000
+
+
+---- pandas code
+-- import pandas as pd
+
+-- def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+--     df = world[(world['area'] >= 3000000) | (world['population'] >= 25000000)]
+--     return df[['name', 'population', 'area']]
